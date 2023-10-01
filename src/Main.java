7913f26200070c465ed3bd02a49356ba7d514b23
nesrinesamali <nesrine.samaali07@gamil.com>
2023-10-01 20:41:26 +0100

    prosit1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         ZooManagement zoo =new ZooManagement();
         System.out.println(zoo.zooName+"comporte"+zoo.nbrCages);
-        }
-    }
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    System.out.println("Veuillez saisir le nombre de cages souhaité : ");
+    int nbrCages = scanner.nextInt();
+
+        scanner.nextLine();
+
+
+        System.out.print("Veuillez saisir le nom du zoo : ");
+    String zooName = scanner.nextLine();
+
+
+    ZooManagement zm = new ZooManagement(nbrCages, zooName);
+
+
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages + " cages");
+
+
+        scanner.close();
+}}
+
+
+
+
+
+
+
+
+
+
+
+
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,4 +1,12 @@
 public class ZooManagement {
     int nbrCages=20;
     String zooName="my zoo";
+    public ZooManagement(int nbrCages, String zooName) {
+        this.nbrCages = nbrCages;
+        this.zooName = zooName;
+    }
+
+    public ZooManagement() {
+
+    }
 }
